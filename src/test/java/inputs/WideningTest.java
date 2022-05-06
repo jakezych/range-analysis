a7063a8ef25a878ba1c3324662b398be8d329cac
Jake Zych <zych.jake@gmail.com>
2022-05-06 18:03:52 -0400

    implemented widening operator tests

---
+++ src/test/java/inputs/WideningTest.java
@@ -0,0 +1,28 @@
+package inputs;
+
+public class WideningTest {
+    // Test to see if the analysis will terminate
+    public static void testTermination() {
+        int x, y;
+        int[] array = new int[5];
+        y = -10;
+        x = 0;
+        while (x != y) {
+            x = x + 1;
+        }
+        y = 0;
+        int ignore = array[y]; // OKAY
+        ignore = array[x]; // WARNING
+    }
+
+    // TODO: Test how it affects the precision
+    public static void testPrecision() {
+
+    }
+
+    // TODO: Test constant value truncation
+    public static void testConstantTruncation() {
+
+    }
+
+}
---
+++ src/test/java/range_analysis/WideningOperatorTest.java
@@ -0,0 +1,29 @@
+package range_analysis;
+
+import common.ErrorMessage;
+import common.Utils;
+import org.junit.Assert;
+import org.junit.Test;
+import soot.Main;
+import soot.PackManager;
+import soot.Transform;
+
+public class WideningOperatorTest extends AnalysisTest {
+    void add_analysis() {
+        analysisName = IntraAnalysisTransformer.ANALYSIS_NAME;
+        PackManager.v().getPack("jap").add(
+                new Transform(analysisName,
+                        IntraAnalysisTransformer.getInstance())
+        );
+    }
+
+    @Test
+    public void testWideningOperator() {
+        addTestClass("inputs.WideningTest");
+        Main.main(getArgs());
+
+        addExpected(ErrorMessage.POSSIBLE_OUT_OF_BOUNDS_INDEX_WARNING, 15);
+        Assert.assertEquals(expected, Utils.getErrors());
+    }
+
+}
